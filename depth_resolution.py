52ad1796f5af2f0defd3b638d53b7a4c54406ddd
{keitarokanda} <{keitaro.kanda.q1@dc.tohoku.ac.jp}>
2023-01-06 18:44:24 +0900

    resolutionの検討

---
+++ depth_resolution.py
@@ -0,0 +1,31 @@
+from cProfile import label
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#誘電率
+epsilon1 = 4.0
+epsilon0 = 1.0
+#光速
+c = 299792458 #[m/s]
+
+
+df = np.arange(1, 200) #MHz
+
+dR_basalt = c/(2*np.sqrt(epsilon1)*df*10**6)
+dR_vacuum = c/(2*np.sqrt(epsilon0)*df*10**6)
+
+
+#描画
+plt.figure(figsize=(8, 8))
+plt.plot(df, dR_basalt, label='in basalt')
+plt.plot(df, dR_vacuum, label='in vacuum')
+
+#グラフの体裁
+plt.title('depth resolution', fontsize='20')
+plt.xlabel('frequency band width [MHz]', fontsize='15')
+plt.ylabel('depth resolution [m]', fontsize='15')
+plt.ylim(0, 5)
+plt.grid()
+plt.legend(fontsize='10')
+plt.show()
